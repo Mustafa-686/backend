944101adfe41a0d37cfe0d7d53397da80bc8ddf6
Mustafa-686 <muhammadmustafa6259@gmail.com>
2025-02-22 17:15:38 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import dotenv from "dotenv";
 import mongoose  from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`SERVER IS RUNNING AT PORT : ${process.env.PORT}`);
+        
+    })
+})
+.catch((Error)=>{
+    console.log("MONGODB CONNECTION LOST !!", Error);
+    
+})
